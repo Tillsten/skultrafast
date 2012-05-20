370fca7128c735fcbc9dd45ea1a5eba8443d6a18
Till Stensitzki <Till Stensitzki@localhost>
2012-05-20 05:17:06 +0200

    working gui! BUG: factor 2 in x_vec, wtf? removed num_exponentials

--- lmtraits.py
+++ lmtraits.py
@@ -5,8 +5,8 @@ Created on Sat May 19 20:25:41 2012
 @author: Tillsten
 """
 import lmfit
-from traits.api import Str, Float, Bool, HasTraits, List, Button
-from traitsui.api import Group, Item, View, TableEditor
+from traits.api import Str, Float, Bool, HasTraits, List, Button, on_trait_change
+from traitsui.api import VGroup, Item, View, TableEditor, HGroup
 from traitsui.table_column import ObjectColumn
 from numpy import inf
 
@@ -21,6 +21,7 @@ class Parameter(HasTraits):
 
 class AllParameter(HasTraits):    
     paras=List(Parameter)
+    add_para=Button('Add')
     apply_paras=Button('Apply')
     start_fit=Button('Fit')
     
@@ -31,38 +32,65 @@ class AllParameter(HasTraits):
           ObjectColumn(name='min'),
           ObjectColumn(name='max')]
     tb.columns=cols
-    traits_view=View(Group(Item('paras', editor=tb, show_label=False),
+    tb.row_factory=Parameter
+    tmp=HGroup(Item('add_para', show_label=False),
+               Item('apply_paras', show_label=False),
+               Item('start_fit', show_label=False))
+    traits_view=View(VGroup(Item('paras', editor=tb, show_label=False),
+                            tmp
                            ),
                      resizable = True,
                      width = .15,
                      height = .2)
+                     
 
+    def _paras_default(self):
+        p1=Parameter(name='x0',value=0.)
+        p2=Parameter(name='w',value=0.1,min=0)
+        p3=Parameter(name='t0',value=1.,min=0)
+        return [p1,p2,p3]
+    
+    def _add_para_fired(self):        
+        name='t'+str(len(self.paras)-2)
+        p=Parameter(name=name,value=10**(len(self.paras)-2),min=0)
+        self.paras.append(p)
+    
+    @on_trait_change('paras[]')
+    def _apply_para_fired(self):
+        pass
+        
     def from_lmparas(self,lmparams):
+        self.paras=[]
         for i in lmparams:
             lm_p=lmparams[i]
             p=Parameter(name=i, value=lm_p.value, 
                         fixed=not lm_p.vary)
-            if lm_p.max:
+            if lm_p.max!=None:
                 p.max=lm_p.max
-            if lm_p.min:
+            if lm_p.min!=None:
                 p.min=lm_p.min
             self.paras.append(p)
             
     def to_lmparas(self):
-        p=lmfit.Parameters
+        p=lmfit.Parameters()
         for i in self.paras:
             p.add(i.name,i.value, not i.fixed)
             if i.max!=inf:
-                p.max=i.max
+                p[i.name].max=i.max
             if i.min!=-inf:
-                p.min=i.min
+                p[i.name].min=i.min
         return p
+        
+    def to_array(self):
+        return [i.value for i in self.paras]
+            
             
 
 
 if __name__=='__main__':
     p=lmfit.Parameters()
     p.add_many(('x0',0),('w',0.1))
+    
     pa=AllParameter()
     pa.from_lmparas(p)
     pa.configure_traits()
