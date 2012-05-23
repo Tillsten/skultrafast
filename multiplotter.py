4654c7f7c28213d582e5c4d5f7c9e38ebd6f3087
Till Stensitzki <mail.till@gmx.de>
2012-05-23 17:54:43 +0200

    some more cleaning, remove_para added

--- multiplotter.py
+++ multiplotter.py
@@ -12,18 +12,22 @@ import time
 
 # ETS imports (non-chaco)
 from enable.component_editor import ComponentEditor
-from traits.api import HasTraits, Instance, Int, List, Str, 
+from enable.api import BaseTool
+from traits.api import HasTraits, Instance, Int, List, Str, Array
 from traitsui.api import Item, View
 
 # Chaco imports
 from chaco.api import ArrayPlotData, Plot
+from chaco.tools.api import PanTool, ZoomTool, RangeSelection, \
+        RangeSelectionOverlay, LegendTool
+
 
 
 from chaco.example_support import COLOR_PALETTE
 
 from chaco.default_colors import palette11 as COLOR_PALETTE
 #from enthought.chaco.example_support import COLOR_PALETTE
-
+COLOR_PALETTE=['blue','green','red','cyan','yellow','pink']
 
 class MultiPlotter(HasTraits):
     plot=Instance(Plot)
@@ -44,7 +48,7 @@ class MultiPlotter(HasTraits):
         
     def _ylabel_changed(self,new):        
         self.plot.y_axis.title=new
-        
+     
     
     def _plot_default(self):
         plot=Plot(self.pd)        
@@ -53,14 +57,11 @@ class MultiPlotter(HasTraits):
         plot.tools.append(PanTool(plot, drag_button='right'))        
         zoom = ZoomTool(component=plot, tool_mode="box", always_on=True)
         plot.overlays.append(zoom)          
-        
         return plot
     
     def _ytemp_changed(self):
         self.pd.set_data('ytemp',self.ytemp)   
-    
-    
-           
+
     def delete_ydata(self):
         self.ydata=[]        
         l=[]
@@ -73,9 +74,8 @@ class MultiPlotter(HasTraits):
     def add_ydata(self,name, y):
         self.ydata.append(y)
         self.pd.set_data(name,y)
-        if len(self.ydata)>0:            
-            c=COLOR_PALETTE[len(self.ydata)%len(COLOR_PALETTE)]
-            self.plot.plot(('x',name),name=name,color=c)
+        c=COLOR_PALETTE[(len(self.ydata)-1)%len(COLOR_PALETTE)]
+        self.plot.plot(('x',name),name=name,color=c,line_width=3.)
         self.plot.request_redraw()
 
         
@@ -87,9 +87,10 @@ if __name__=='__main__':
     x=np.linspace(0,10,100)
     y=np.sin(x)         
     mlp=MultiPlotter(xaxis=x)
-    mlp.ydata.append(y)
+    mlp.add_ydata('sin',y)
+    
     mlp.xlabel='dsfj'    
-    mlp.delete_ydata()
+    #mlp.delete_ydata()
     mlp.configure_traits()
     
-    #mlp.ydata.append(y)
+  
