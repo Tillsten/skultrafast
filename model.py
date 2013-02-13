d3adc9a4c900bff702821369d0eed55a497167a0
Till Stensitzki <mail.till@gmx.de>
2013-02-13 20:21:23 +0100

    added kinetic modeling and spectral modeling (in part)

--- model.py
+++ model.py
@@ -7,20 +7,8 @@ Created on Mon Jan 28 16:15:03 2013
 import numpy as np
 import networkx as nx
 from sklearn.linear_model import ridge_regression
-
-def make_A(tau):
-    g=nx.DiGraph()
-    g.add_edge('S1_hot', 'S1_warm', tau=tau[0])    
-    g.add_edge('S1_warm', 'S1', tau=tau[1])
-    g.add_edge('S1', 'S0', tau=6000)
-    g.add_edge('S1', 'T2', tau=tau[2])
-    #g.add_edge('T2','T1', tau=tau[3])
-    #g.add_edge('S1', 'S0', tau=tau[2])
-    a0 = nx.to_numpy_matrix(g, weight='tau')
-    a = np.where(a0==0, 0, 1/a0)    
-    a = a.T - np.diag(np.sum(a, 1).flat)
-    return a, g
-   
+from skultrafast.kinetic_model import Model
+import lmfit
 
 
 def sol_matexp(A, tlist, y0):
@@ -42,21 +30,19 @@ def make_state_spectra(para, x, state, shape='lorentz'):
 
 def fit(p, x, mode='lm', has_spectral=True):
     taus = [p[name].value for name in p if name.startswith('tau')]
-    a, g = make_A(taus)       
-    y0 = np.zeros(a.shape[0])
-    states =  g.nodes()
-    y0[states.index('S1_hot')] += 1       
-    A = sol_matexp(a, f.t, y0)
-    s0 = states.index('S0')
+    print taus    
+    A = func(taus, f.t)    
+    states = m.get_compartments()    
     if has_spectral:
         shapes = np.zeros((len(states), len(x)))
         for i, st in enumerate(states):        
             shapes[i, :] = make_state_spectra(p, x, st)
         bleach = make_state_spectra(p, x, 'bleach')
-        bleach2 = make_state_spectra(p, x, 'bleach2')
-        bleach += bleach2
-        shapes = shapes + bleach
-        shapes[s0, :] -= bleach
+        bleach += p['const'].value
+        #bleach2 = make_state_spectra(p, x, 'bleach2')
+        #bleach += bleach2
+        shapes[:-1,:] += bleach
+        
         fit = A.dot(shapes)
         res = fit - f.data
         
@@ -68,39 +54,69 @@ def fit(p, x, mode='lm', has_spectral=True):
     if mode=='sum':
         return (res*res).sum() #+ 15*np.abs(np.diff(c, 2, 1)).sum()
     elif mode=='p':
-        return res, A, g, shapes
+        return res, A, shapes
     elif mode=='lm':        
         o = res.flatten()
-        o = np.array(o)[:]
-        print o.shape
+        o = np.array(o)[:]        
         #o[-1] += 0.01*c.sum()  
         return o
 
-def add_state(p, state, A, w, xc, min=0, vary=True):
+#def make_A(tau):
+#    g=nx.DiGraph()
+#    g.add_edge('S1hot', 'S1warm', tau=tau[0])    
+#    g.add_edge('S1hot', 'T1', tau=tau[1])
+#    g.add_edge('S1warm', 'S1', tau=tau[2])
+#    g.add_edge('S1', 'S0', tau=7000)
+#    #g.add_edge('S1', 'T2', tau=tau[2])
+#    #g.add_edge('T2','T1', tau=tau[3])
+#    #g.add_edge('S1', 'S0', tau=tau[2])
+#    a0 = nx.to_numpy_matrix(g, weight='tau')
+#    a = np.where(a0==0, 0, 1/a0)    
+#    a = a.T - np.diag(np.sum(a, 1).flat)
+#    return a, g
+#   
+m = Model()
+m.add_trans('S1hot', 'S1warm')
+#m.add_trans('S1hot', 'T1')
+m.add_trans('S1warm', 'S1')
+m.add_trans('S1', 'T1')
+m.add_trans('T1', 'S0')
+#m.add_trans('S0', 'S0')
+print m.get_compartments()
+y0 = np.array([1., 0, 0, 0, 0])[:, None]
+func = m.get_func(y0)
+a = func(np.array([7., 8, 15, 6600]), f.t)
+plot(f.t, a)
+def add_state(p, state, A, w, xc, min=20, vary=True):
     p.add(state + '_A', A, min=min, vary=vary)
-    p.add(state + '_w', w, min=0, vary=vary)
+    p.add(state + '_w', w, min=2, vary=vary)
     p.add(state + '_xc', xc, min=0, vary=vary)
 
 import scipy.optimize as opt
-x0 = [2, 5, 100, 5]
+x0 = [5, 7, 15., 6600.]
 import lmfit
 p = lmfit.Parameters()
 
 for i, tau in enumerate(x0):
-    p.add('tau'+str(i), tau, min=0)
-add_state(p, 'S1_hot', 40, 4, 1516)
-add_state(p, 'S1_warm', 45, 4, 1519)
-add_state(p, 'T1', 50, 4, 1515)
-add_state(p, 'T2', 40, 4, 1515)
+    p.add('tau'+str(i), tau, min=0, vary=True)
+p['tau3'].vary = False
+add_state(p, 'S1hot', 30 , 4, 1516)
+add_state(p, 'S1warm', 45, 4, 1519)
+add_state(p, 'T1', 30, 4, 1515)
+#add_state(p, 'T2', 40, 4, 1517)
 add_state(p, 'S1', 45, 4, 1520)
 add_state(p, 'S0', 0, 0, 0, vary=False)
-add_state(p, 'bleach', -40, 6, 1522, min=None)
-add_state(p, 'bleach2', -0, 6, 1505, min=None)
-
+add_state(p, 'bleach', -40, 4, 1522, min=None)
+#add_state(p, 'bleach2', -3, 60, 1505, min=None)
+p.add('const', -3)
 mi = lmfit.Minimizer(fit, p, [f.wl])
 
-mi.leastsq()
-lmfit.report_errors(p)
-
-
-res, A, g, shapes = fit(p, f.wl, 'p')
+mi.scalar_minimize('CG')
+#
+lmfit.report_errors(p, show_correl=False)
+res, A, shapes = fit(p, f.wl, 'p')
+print sum(res**2)
+subplot(121)
+plot(f.wl, shapes.T)
+subplot(122)
+plot(f.t, A)
