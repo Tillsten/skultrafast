c87addcdcd3647eebb3889ab29f75774c4b43106
tillsten <tillsten@localhost>
2013-04-17 17:15:37 +0200

    small fixes, moved base_function to an extra directory

--- fitter.py
+++ fitter.py
@@ -17,213 +17,7 @@ try:
 except ImportError:
     has_sklearn = False
 
-sq2 = float(np.sqrt(2))
-from numba import autojit, vectorize, f8, jit
-from math import exp, erfc
-
-
-@autojit
-def _coh_gaussian(t, w, tz):
-    """
-    Models coherent artifacts proportional to a gaussian and it's first three derivatives.
-
-    Parameters
-    ----------
-    t:  ndarray
-        2d - Array containing the time-coordinates
-    w:  float
-        The assumed width/sq2
-    tz: float
-        The assumed time zero.
-
-    Returns
-    -------
-    y:  ndarray (shape(t), 4)
-        Array containing a gaussian and it the scaled derivatives,
-        each in its own column.
-    """
- 
-    w = w / 1.4142135623730951
-    ta = t + tz
-    n, m = ta.shape        
-    y = np.zeros(( n, m, 4))
-    for i in range(n):
-        for j in range(m):
-            tt = ta[i, j]
-            if tt/w < 2.5:
-                y[i, j, 0] = np.exp(-0.5 * (tt / w)* (tt / w)) / (w * np.sqrt(2 * 3.14159265))    
-                y[i, j, 1] = y[i, j, 0] * (-tt / w / w)
-                y[i, j, 2] = y[i, j, 0] * (tt * tt / w / w  / w / w - 1 / w /w)
-                y[i, j, 3] = y[i, j, 0] * (-tt ** 3 / w ** 6 + 3 * tt / w ** 4)
-            
-    y /= np.max(np.abs(y), 0)
-    return y
-
-t_array = np.subtract.outer(np.linspace(-2,4, 200), np.zeros(5))   
-w = 0.1
-a= _coh_gaussian(t_array, w, 0.)
-   
-
-@jit('f8(f8)', nopython=True)
-def fast_erfc(x):
-    """
-    Calculates the erfc near zero faster than
-    the libary function, but has a bigger error, which 
-    is not a problem for us.
-    
-    Parameters
-    ----------   
-    x: float
-        The array
-    
-    Returns
-    -------
-    ret: float
-        The erfc of x.
-    """
-    a1 = 0.278393 
-    a2 = 0.230389 
-    a3 = 0.000972 
-    a4 = 0.078108    
-    smaller = x < 0
-    if smaller:
-        x = x * -1.
-    bot = 1 + a1*x + a2*x*x +a3*x*x*x + a4*x*x*x*x
-    ret = 1./(bot*bot*bot*bot)    
-    
-    if smaller:
-        return -ret + 2.
-    else: 
-        return  ret 
-    
-
-@jit('f8(f8, f8, f8, f8)', nopython=True)
-def folded_fit_func(t, tz, w, k):    
-    """
-    Returns the value of a folded exponentials. 
-    Employs some domain checking for making the calculation.
-    
-    Parameters
-    ----------
-    t: float
-        The time.
-    tz: float
-        Timezero.
-    w:
-        Width of the gaussian system response.
-    k:
-        rate of the decay.
-    """
-    t = t - tz                
-    if t < -2.5 * w:
-        return 0.
-    elif t < 2.5 * w:
-        #print -t/w + w*k/2., w, k, t
-        return exp(k * (w*w*k/4.0 - t)) * 0.5 * fast_erfc(-t/w + w*k/2.)
-    elif t < 5./k:
-        return exp(k* (w*w*k/ (4.0) - t))
-    else:
-        return 0.
-
-@jit(f8[:, :, :], [f8[:, :], f8, f8, f8[:]])        
-def _fold_exp(t_arr, w, tz, tau_arr):
-    
-    """
-    Returns the values of the folded exponentials for given parameters.
-
-    Parameters
-    ----------
-    t_arr:  ndarray(N)
-        Array containing the time-coordinates
-    w:  float
-        The assumed width/sq2
-    tz: float
-        The assumed time zero.
-    tau_arr: ndarray(M)
-        The M-decay rates.
-
-    Returns
-    -------
-    y: ndarray
-       Folded exponentials for given taus.
-    """    
-    n, m = t_arr.shape
-    l = tau_arr.size
-    out = np.empty((l, m, n))
-    for tau_idx in range(l):
-        k = 1 / tau_arr[tau_idx]        
-        for j in range(m):
-            for i in range(n):            
-                out[tau_idx, j, i] = folded_fit_func(t_arr[i, j], -tz, w, k)                  
-    return out.T
-
-#    
-
-    
-#
-#
-#def _fold_ndgaussian(base, w_in_steps, ):
-#    """
-#    Using scipy ndimage gaussian filter for convolution.
-#    
-#    Parameter
-#    ---------
-#    base: ndarray(N, M, K)
-#        vectors to be convoluted along the 
-#        
-#    """
-#    from scipy.ndimage import gaussian_filter1d
-#    return gaussian_filter1d()
-    
-#@jit(f8[:, :, :], [f8[:, :], f8, f8, f8[:]])
-def _exp(tt, w, tz, tau):
-    """
-    Returns the values of the exponentials for given parameters.
-
-    Parameters
-    ----------
-    tt:  ndarray(N)
-        Array containing the time-coordinates
-    w:  float
-        The assumed width/sq2
-    tz: float
-        The assumed time zero.
-    tau: ndarray(M)
-        The M-decay rates.
-
-    Returns
-    -------
-    y: ndarray
-       Exponentials for given tau's.
-    """
-    t = tt + tz
-    y = np.exp(-t / (tau[:, None]))
-    return y
-
-#@autojit
-#def _coh_gaussian(t, w, tz):
-
-#    w = w / sq2
-#    tt = t + tz
-#        
-#    for i in range(t.shape[0]):
-#        if tt[i, -1] > w*4.:
-#            break
-#    print i
-#    t_idx = i
-#    #y = np.where(tt < 4 * w, np.exp(-0.5 * (tt / w) ** 2) / (w * np.sqrt(2 * 3.14159265)), 0)    
-#    y = np.zeros_like(tt)
-#    y[:, :t_idx] = np.exp(-0.5 * (tt[:, :t_idx] / w) ** 2) / (w * np.sqrt(2 * 3.14159265))    
-#    y = np.tile(y[..., None], (1, 1, 4))
-#    y[:, :t_idx, 1] *= (-tt[:, :t_idx] / w ** 2)
-#    y[:, :t_idx, 2] *= (tt[:, :t_idx] ** 2 / w ** 4 - 1 / w ** 2)
-#    y[:, :t_idx, 3] *= (-tt[:, :t_idx] ** 3 / w ** 6 + 3 * tt[:, :t_idx] / w ** 4)
-#    y /= np.max(np.abs(y), 1)
-#    return y
-
-
-#plot(np.linspace(-2,4, 200), _coh_gaussian1(t_array, w, 0)[:, 0, :], 'r')
-#plot(np.linspace(-2,4, 200), a[:, 0, :], 'k')
+from skultrafast.base_functions import _fold_exp, _coh_gaussian
 
 def solve_mat(A, b_mat, method='fast'):
     """
@@ -233,7 +27,7 @@ def solve_mat(A, b_mat, method='fast'):
         return linalg.solve(A.T.dot(A), A.T.dot(b_mat), sym_pos=True)
 
     elif method == 'ridge':
-        alpha = 0.0001
+        alpha = 0.001
         X = np.dot(A.T, A)
         X.flat[::A.shape[1] + 1] += alpha
         Xy = np.dot(A.T, b_mat)
@@ -345,6 +139,7 @@ class Fitter(object):
             para = para[self.model_disp:]
         self.num_exponentials = self.last_para.size - self.model_disp - 1
         self._build_xvec(para)
+        self.x_vec = np.nan_to_num(self.x_vec)
         self.c = solve_mat(self.x_vec, self.data, self.lsq_method)
         self.model = np.dot(self.x_vec, self.c)
         self.c = self.c.T
@@ -368,11 +163,9 @@ class Fitter(object):
             tau_idx = idx[1:]
 
         if any(idx[:2]) or self.model_disp or True:
-
             if self.model_coh:
                 x_vec = np.zeros((self.t.size, self.num_exponentials + 4))
-                x_vec[:, -4:] = _coh_gaussian(self.t[:, None], w, x0).squeeze()
-                print para, taus
+                x_vec[:, -4:] = _coh_gaussian(self.t[:, None], w, x0).squeeze()                
                 x_vec[:, :-4] = _fold_exp(self.t[:, None], w,
                                                x0, taus).squeeze()
             else:
