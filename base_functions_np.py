cef33e9d72847cf9372f62113bb65ae349ecc65e
tillsten <mail.till@gmx.de>
2013-08-13 11:11:39 +0200

    more ltmgui work

---
+++ base_functions_np.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 15:38:47 2013
+
+@author: Tillsten
+"""
+import numpy as np
+from scipy.special import erfc
+
+def _fold_exp(tt, w, tz, tau):
+    """
+    Returns the values of the folded exponentials for given parameters.
+
+    Parameters
+    ----------
+    tt:  ndarray(N)
+        Array containing the time-coordinates
+    w:  float
+        The assumed width/sq2
+    tz: float
+        The assumed time zero.
+    tau: ndarray(M)
+        The M-decay rates.
+
+    Returns
+    -------
+    y: ndarray
+       Folded exponentials for given taus.
+       
+    """
+    ws = w
+    k = 1 / (tau[..., None, None])
+    t = (tt + tz).T[None, ...]
+    y = np.exp(k * (ws * ws * k / (4.0) - t))
+    y *= 0.5 * erfc(-t / ws + ws * k / (2.0))
+    return y.T
+
+def _coh_gaussian(t, w, tz):
+    """Models artifacts proportional to a gaussian and it's derivatives
+
+    Parameters
+    ----------
+    t:  ndarray
+        Array containing the time-coordinates
+    w:  float
+        The assumed width/sq2
+    tz: float
+        The assumed time zero.
+
+    Returns
+    -------
+    y:  ndarray (len(t), 4)
+        Array containing a gaussian and it the scaled derivatives,
+        each in its own column.
+    """
+    w = w / sq2
+    tt = t + tz
+    y = np.where(tt/w < 4, exp(-0.5 * (tt / w) ** 2) / (w * sqrt(2 * 3.14159265)), 0)
+    y = np.tile(y[..., None], (1, 1, 4))
+    y[..., 1] *= (-tt / w ** 2)
+    y[..., 2] *= (tt ** 2 / w ** 4 - 1 / w ** 2)
+    y[..., 3] *= (-tt ** 3 / w ** 6 + 3 * tt / w ** 4)
+    y /= np.max(np.abs(y), 0)
+    return y
---
+++ kinetic_model.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+This modul build a kinetic matrix from given transtions between 
+compartments.
+"""
+import sympy, cython
+import numpy as np
+
+
+class Transition(object):
+    """
+    Represents a transtion between comparments.
+    """
+    def __init__(self, from_comp, to_comp, 
+                 tau=None, rate=None, qu_yield=None):         
+        self.rate = sympy.Symbol(from_comp + '_' + to_comp, real=True, 
+                                 positive=True)
+        self.from_comp = from_comp
+        self.to_comp = to_comp
+        self.qu_yield = qu_yield or 1. 
+
+
+class Model(object):
+    """
+    Helper class to make a model
+    """
+    def __init__(self):
+        self.transitions = []
+    
+    def add_trans(self, *args, **kwargs):
+        """
+        Creates a transiton and adds it to self
+        """
+        trans = Transition(*args, **kwargs)
+        self.transitions.append(trans)
+        
+    def build_matrix(self):
+        """model.add_trans('d', 'e')
+        Builds the n x n k-matrix
+        """
+        comp = get_comparments(self.transitions)
+        idx_dict = dict(enumerate(comp))
+        inv_idx = dict(zip(idx_dict.values(), idx_dict.keys()))        
+        mat = sympy.zeros((len(comp), len(comp)))
+        #for i, comp in idx_dict.iteritems():                     
+        for t in self.transitions:
+            i = inv_idx[t.from_comp]
+            mat[i, i] = mat[i, i] - t.rate
+            mat[inv_idx[t.to_comp], i] = t.rate 
+            if t.qu_yield != 1. :
+                mat[inv_idx[t.to_comp], i] = t.rate * t.qu_yield
+        self.mat = mat
+        return mat
+    
+    def get_compartments(self):
+        return get_comparments(self.transitions)
+        
+    def get_func(self, y0):
+        """
+        Gives back a function (compiled with cython)
+        """        
+        A = self.build_matrix()
+        ts = sympy.Symbol('ts', real=True)   
+        expA = (A*ts).exp()            
+        fun = expA * y0          
+        r, e = sympy.cse(fun)
+        e_sim = [i.simplify() for i in e]
+        prog_str = _make_progstr(self.transitions, fun, e, r)
+        self.prog_str = prog_str
+        self.fun = fun
+        self.expA = expA
+        #print prog_str
+        def sol_fun(p, t):
+            ret = cython.inline(prog_str)
+            return ret
+        return sol_fun #autowrap(fun,'C', backend='CYTHON', tempdir='..')
+
+    
+    def get_trans(self, y0, taus, t):
+        """
+        Return the solution
+        """
+        import scipy.linalg as la
+        symbols = get_symbols(self.transitions)
+                
+        k = np.array(self.mat.subs(zip(symbols, taus))).astype('float')
+        
+        o = np.zeros((len(t), k.shape[0]))
+        
+        for i in xrange(t.shape[0]):
+            o[i, :] = la.expm(k * t[i]).dot(y0)[:, 0]
+        
+        return o
+       #pass  print mat.
+
+import scipy.linalg as la
+    
+
+def _make_appy_sym(sym):
+    l = []
+    for i, s in enumerate(sym):
+        l.append(str(s) + '= 1/p[{}]\n'.format(i))
+    return ''.join(l)
+        
+def get_comparments(list_trans):
+    """
+    Getting a list of transtions, return the compartments
+    """
+    l = []
+    for trans in list_trans:
+        if trans.from_comp not in l:
+            l.append(trans.from_comp)
+        if trans.to_comp not in l:
+            l.append(trans.to_comp)
+    return l
+    
+def get_symbols(list_trans):
+    """
+    Return the used symbols
+    """
+    return [t.rate for t in list_trans]
+    
+if __name__ == '__main__':
+    model = Model()
+    model.add_trans('a', 'b')
+    model.add_trans('b', 'c')
+    model.add_trans('c', 'd')
+    model.add_trans('d', 'e')
+    model.add_trans('e', 'e')
+    #model.add_trans('S1', 'S0', tau=6600)
+    t = np.linspace(1, 100, 1000)
+    y0 = np.array([1, 0, 0, 0, 0])[:, None]
+    #plot(t, model.exp_solution(y0, t))
+    print get_comparments(model.transitions)
+    print model.build_matrix()
+    
+    #fu = model.get_func(y0)
+    tau = np.array([1., 10., 100, 300, 10000, 10000])
+    model.get_trans(y0, tau, t)
