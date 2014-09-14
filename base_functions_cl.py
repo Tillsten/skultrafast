0fa6e44320cfc6b6554edfc277bac21261504cb4
Till Stensitzki <mail.till@gmx.de>
2014-09-14 18:01:20 +0200

    added ir and plot helpers

--- base_functions_cl.py
+++ base_functions_cl.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 import pyopencl as cl
-import pyopencl.array as cl_array    
+import pyopencl.array as cl_array
 
 mf = cl.mem_flags
 #PYOPENCL_COMPILER_OUTPUT=1
 plat = cl.get_platforms()[-1]
 dev = plat.get_devices()[0]
+print dev, plat
 ctx = cl.Context([dev])
 queue = cl.CommandQueue(ctx)
 work_size = (128, 1)
@@ -22,9 +23,9 @@ prg = cl.Program(ctx, """
 float inline fast_erfc(float x)
     {
     uint smaller = 0;
-    float bot; 
+    float bot;
     float ret;
-    if (x<0) 
+    if (x<0)
         {
          x = x * -1.;
          smaller = 1;
@@ -33,10 +34,10 @@ float inline fast_erfc(float x)
     ret = 1./(bot*bot*bot*bot);
 
     if (smaller)  {return -ret + 2.;}  {return  ret;}
-    }    
-    
- __kernel void fold_exp(__global const float *t, const float w, 
-                        __global const float *k,  __global float *z, 
+    }
+
+ __kernel void fold_exp(__global const float *t, const float w,
+                        __global const float *k,  __global float *z,
                         const int max_gid)
     {
       int gid = get_global_id(0);
@@ -45,10 +46,10 @@ float inline fast_erfc(float x)
       int idx = gid * len_tau + gid2;
 
       if (gid > max_gid) {return;};
-      
+
       if (t[gid]< -w*2.5){z[idx] = 0;}
           {z[idx] = native_exp(k[gid2]*(w*w*k[gid2]*.25f - t[gid]));};
-      if (t[gid]< 2.5 * w) {z[idx] *= .5* erfc(-t[gid]/w + w*k[gid2]*0.5);};    
+      if (t[gid]< 2.5 * w) {z[idx] *= .5* erfc(-t[gid]/w + w*k[gid2]*0.5);};
     }
 """).build()
 
@@ -63,10 +64,10 @@ prg2 = cl.Program(ctx, """
       int gid2 = get_global_id(1);
       int len_tau = get_global_size(1);
       int idx = gid * len_tau + gid2;
-      
+
       float tt = t[gid];
-      
-      
+
+
       if ((t[gid] < -w*4.) || (w*4. < t[gid])){z[idx] = 0.;}
           {
               z[idx] = native_exp(-.5f * (tt / w) * (tt / w)) ;
@@ -80,27 +81,27 @@ prg2 = cl.Program(ctx, """
 
 prg3 = cl.Program(ctx, """
 
-__kernel void multi_dot(__global const float *model, 
-                        __global const float *coeff, 
-                        __global float *out, 
+__kernel void multi_dot(__global const float *model,
+                        __global const float *coeff,
+                        __global float *out,
                         const int n, const int m)
     {
       int y = get_global_id(0);
       int x = get_global_id(1);
       int sizeX = get_global_size(1);
       int sizeY = get_global_size(0);
-      
+
       int m_idx;
-      
-            
-      for (int i=0; i<n; i++)           
+
+
+      for (int i=0; i<n; i++)
           {
-          m_idx = i * sizeX * sizeY + sizeX * y + x;        
+          m_idx = i * sizeX * sizeY + sizeX * y + x;
           //printf("i %d x %d y %d %f %d\\n", i,x, y,  model[m_idx], m_idx);
           //printf("coeff %d %f\\n", x + sizeX * i, coeff[x*n + i]);
           out[x + y * sizeX] += model[m_idx] * coeff[x + sizeX * i];};
     }
-    
+
 """).build()
 
 
@@ -108,69 +109,69 @@ __kernel void multi_dot(__global const float *model,
 def _coh_gaussian(t_array, w, tz):
     if tz != 0.:
         t_array -= tz
-    
-    shape = t_array.shape    
-    t_array = t_array.astype(np.float32)    
+
+    shape = t_array.shape
+    t_array = t_array.astype(np.float32)
     t_arr_gpu = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=t_array)
     shape = (shape[0], shape[1], 4)
-    out = cl_array.empty(queue, shape=shape, dtype=np.float32)  
-    
-    prg2.coh_gauss(queue, (t_array.size, 4), None, t_arr_gpu,  np.float32(w/1.4142), out.data).wait()    
-    
+    out = cl_array.empty(queue, shape=shape, dtype=np.float32)
+
+    prg2.coh_gauss(queue, (t_array.size, 4), None, t_arr_gpu,  np.float32(w/1.4142), out.data).wait()
+
     a = out.get()
     a /= np.abs(a).max(0)
     return a
 
-def _fold_exp(t_array, w, tz, tau_arr):    
+def _fold_exp(t_array, w, tz, tau_arr):
     if tz != 0.:
-        t_array -= tz
-    
-    shape = t_array.shape    
+        t_array = t_array - tz
+
+    shape = t_array.shape
     t_array = t_array.astype(np.float32)
-    
+
     t_arr_gpu = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=t_array)
-    tau_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, 
+    tau_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR,
                         hostbuf=(1/tau_arr).astype(np.float32))
     shape = (shape[0], shape[1], tau_arr.size)
-    out = cl_array.empty(queue, shape=shape, dtype=np.float32)    
-    global_work_size = t_array.size + (work_size[0] - t_array.size % work_size[0]) 
-    prg.fold_exp(queue, (global_work_size, tau_arr.size), work_size, t_arr_gpu, np.float32(w), 
-                 tau_buf, out.data, np.uint32(t_array.size)).wait()    
-    
+    out = cl_array.empty(queue, shape=shape, dtype=np.float32)
+    global_work_size = t_array.size + (work_size[0] - t_array.size % work_size[0])
+    prg.fold_exp(queue, (global_work_size, tau_arr.size), work_size, t_arr_gpu, np.float32(w),
+                 tau_buf, out.data, np.uint32(t_array.size)).wait()
+
     a = out.get()
 
     return a
-    
+
 def _fold_exp_and_coh(t_array, w, tz, tau_arr):
     if tz != 0.:
         t_array -= tz
-    
-    shape = t_array.shape    
+
+    shape = t_array.shape
     t_array = t_array.astype(np.float32)
-    
+
     t_arr_gpu = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=t_array)
-    tau_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, 
+    tau_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR,
                         hostbuf=(1/tau_arr).astype(np.float32))
     shape = (shape[0], shape[1], tau_arr.size)
     shape_coh = (shape[0], shape[1], 4)
-    out = cl_array.empty(queue, shape=shape, dtype=np.float32)    
-    out_coh = cl_array.empty(queue, shape=shape_coh, dtype=np.float32)    
-    
-    global_work_size = t_array.size + (work_size[0] - t_array.size % work_size[0]) 
-    
-    prg.fold_exp(queue, (global_work_size, tau_arr.size), work_size, t_arr_gpu, np.float32(w), 
+    out = cl_array.empty(queue, shape=shape, dtype=np.float32)
+    out_coh = cl_array.empty(queue, shape=shape_coh, dtype=np.float32)
+
+    global_work_size = t_array.size + (work_size[0] - t_array.size % work_size[0])
+
+    prg.fold_exp(queue, (global_work_size, tau_arr.size), work_size, t_arr_gpu, np.float32(w),
                  tau_buf, out.data, np.uint32(t_array.size))
-    
-    prg2.coh_gauss(queue, (global_work_size, 4), work_size, t_arr_gpu,  
-                   np.float32(w/1.4142), out_coh.data, np.uint32(t_array.size)) 
-    
+
+    prg2.coh_gauss(queue, (global_work_size, 4), work_size, t_arr_gpu,
+                   np.float32(w/1.4142), out_coh.data, np.uint32(t_array.size))
+
     queue.finish()
     a = out.get(async=True)
     b = out_coh.get(async=True)
-    
+    a /= np.abs(a).max(0)
     queue.finish()
     return a, b
-    
+
 t_array = np.subtract.outer(np.linspace(-1, 5, 128*4),
                             np.linspace(3, 3, 256*4))
 
@@ -185,43 +186,22 @@ t_array = np.subtract.outer(np.linspace(-1, 5, 128*4),
 #c = np.random.rand(*a.shape[1:])
 ##c = np.ones(a.shape[1:])
 ##c.flat[1] = 2
-#a_gpu = cl_array.to_device(queue, a.astype(np.float32))    
-##c_gpu = cl_array.to_device(queue, c.astype(np.float32))    
+#a_gpu = cl_array.to_device(queue, a.astype(np.float32))
+##c_gpu = cl_array.to_device(queue, c.astype(np.float32))
 ##out = cl_array.zeros(queue, shape=(a.shape[0], a.shape[1]), dtype=np.float32)
 ###print a
-##prg3.multi_dot(queue, out.shape, None, a_gpu.data, c_gpu.data, 
+##prg3.multi_dot(queue, out.shape, None, a_gpu.data, c_gpu.data,
 ##               out.data, np.uint32(a_gpu.shape[-1]), np.uint32(30)).wait()
 ##ax = out.get()
-#               
+#
 def multi_dot(a_gpu, c):
-    #a_gpu = cl_array.to_device(queue, a.astype(np.float32))    
-    c_gpu = cl_array.to_device(queue, c.astype(np.float32))    
+    #a_gpu = cl_array.to_device(queue, a.astype(np.float32))
+    c_gpu = cl_array.to_device(queue, c.astype(np.float32))
     out = cl_array.empty(queue, shape=(a_gpu.shape[0], a_gpu.shape[1]), dtype=np.float32)
 
-    prg3.multi_dot(queue, out.shape, (128,1), a_gpu.data, c_gpu.data, 
+    prg3.multi_dot(queue, out.shape, (128,1), a_gpu.data, c_gpu.data,
                out.data, np.uint32(a_gpu.shape[-1]), np.uint32(30)).wait()
     ax = out.get()
     return ax
 
-#import time
-#from numpy.core.umath_tests import inner1d
-#t = time.time()              
-#multi_dot(a_gpu, c)
-#print (time.time() - t)*1000.
-#
-#t = time.time()              
-#inner1d(a, c)
-#print (time.time() - t)*1000.
-#
-#
-##
-#
-#    
-#t_array = np.subtract.outer(np.linspace(-1, 5, 601),
-#                            np.linspace(3, 3, 401))
-
-#from skultrafast.base_functions import _fold_exp as _fold_exp2
-#from skultrafast.base_functions import _coh_gaussian as coh_g2
-#a,b = _fold_exp_and_coh(t_array, 0.1, 0., np.array([1., 10. ,1000., 10]))
-##coh = _coh_gaussian(t_array, 0.5, 0.)
 ##coh2 = coh_g2(t_array, 0.5, 0)
