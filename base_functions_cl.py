ec273fc45a597ed3a0928e0c2fb8701e7f0518fa
tillsten <mail.till@gmx.de>
2014-10-17 23:53:23 +0200

    small fix

--- base_functions_cl.py
+++ base_functions_cl.py
@@ -169,6 +169,7 @@ def _fold_exp_and_coh(t_array, w, tz, tau_arr):
     a = out.get(async=True)
     b = out_coh.get(async=True)
     a /= np.abs(a).max(0)
+    b /= np.abs(b).max(0)
     queue.finish()
     return a, b
 
