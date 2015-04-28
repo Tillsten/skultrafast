daa469440eaadec892ccd82034b5f833eaa60bb2
tillsten <mail.till@gmx.de>
2015-04-28 18:57:13 +0200

    update savgol filter to use sig, add recalc_wl para reorder ir_helper plot_helper update

--- dv.py
+++ dv.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from scipy.interpolate import splrep, splev
+import scipy.stats as st
+import scipy.signal as sig
 from collections import namedtuple
 
 tup = namedtuple('tup','wl t data')
@@ -25,14 +27,16 @@ def cm2fs(cm):
     return  1/(cm * 3e-5)
 
 def trimmed_mean(arr, axis=-1, ratio=2., use_sem=True):
+    arr = arr.copy()
     std = np.std(arr, axis, keepdims=1)
-    mean = np.median(arr, axis, keepdims=1)  
+    std = np.std(st.trimboth(arr, 0.2, axis), keepdims=1)
+    mean = np.mean(st.trimboth(arr, 0.2, axis), keepdims=1)
     idx = np.abs(arr - mean) > ratio * std
     n = np.sqrt(np.sum(~idx, axis))
     if not use_sem:
         n = 1
     arr[idx] = np.nan
-    
+    print np.sum(idx)/arr.shape[1]
     mean = np.nanmean(arr, axis)
     std = np.nanstd(arr, axis, ddof=1)/n
     return mean, std
@@ -128,7 +132,11 @@ def polydetrend(x, t=None, deg=3):
     p = np.polyfit(t, x, deg)
     yf = np.poly1d(p)(t)
     return x - yf
-    
+
+def exp_detrend(y, t, start_taus=[1], use_constant=True):
+    m, yf = exp_fit(t, y, start_taus, use_constant=use_constant, verbose=0)
+    return y - yf
+
 def arr_polydetrend(x, t=None, deg=3):
     out = np.zeros_like(x)
     for i in range(x.shape[1]):
@@ -142,86 +150,24 @@ def meaner(dat, t, llim, ulim):
 def legend_format(l):
     return [str(i/1000.)+ ' ps' for i in l]
 
-def savitzky_golay(y, window_size, order, deriv=0):
-    """Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
-    The Savitzky-Golay filter removes high frequency noise from data.
-    It has the advantage of preserving the original shape and
-    features of the signal better than other types of filtering
-    approaches, such as moving averages techhniques.
-    Parameters
-    ----------
-    y : array_like, shape (N,)
-        the values of the time history of the signal.
-    window_size : int
-        the length of the window. Must be an odd integer number.
-    order : int
-        the order of the polynomial used in the filtering.
-        Must be less then `window_size` - 1.
-    deriv: int
-        the order of the derivative to compute (default = 0 means only smoothing)
-    Returns
-    -------
-    ys : ndarray, shape (N)
-        the smoothed signal (or it's n-th derivative).
-    Notes
-    -----
-    The Savitzky-Golay is a type of low-pass filter, particularly
-    suited for smoothing noisy data. The main idea behind this
-    approach is to make for each point a least-square fit with a
-    polynomial of high order over a odd-sized window centered at
-    the point.
-    Examples
-    --------
-    t = np.linspace(-4, 4, 500)
-    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
-    ysg = savitzky_golay(y, window_size=31, order=4)
-    import matplotlib.pyplot as plt
-    plt.plot(t, y, label='Noisy signal')
-    plt.plot(t,files=glob.glob(name+'*dat?.npy') np.exp(-t**2), 'k', lw=1.5, label='Original signal')
-    plt.plot(t, ysg, 'r', label='Filtered signal')
-    plt.legend()
-    plt.show()
-    References
-    ----------
-    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
-       Data by Simplified Least Squares Procedures. Analytical
-       Chemistry, 1964, 36 (8), pp 1627-1639.
-    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
-       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannerypsd1D = radialProfile.azimuthalAverage(psd2D)
-       Cambridge University Press ISBN-13: 9780521880688
-    """
-    try:
-        window_size = np.abs(np.int(window_size))
-        order = np.abs(np.int(order))
-    except ValueError, msg:
-        raise ValueError("window_size and order have to be of type int")
-    if window_size % 2 != 1 or window_size < 1:
-        raise TypeError("window_size size must be a positive odd number")
-    if window_size < order + 2:
-        raise TypeError("window_size is too small for the polynomials order")
-    order_range = range(order+1)
-    half_window = (window_size -1) // 2
-    # precompute coefficients
-    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
-    m = np.linalg.pinv(b).A[deriv]
-    # pad the signal at the extremes with
-    # values taken from the signal itself
-    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
-    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
-    y = np.concatenate((firstvals, y, lastvals))
-    return np.convolve( m, y, mode='valid')
 
 def apply_sg(y, window_size, order, deriv=0):
     out = np.zeros_like(y)
+    coeffs = sig.savgol_coeffs(window_size, order, deriv=0, use='dot')
     for i in range(y.shape[1]):
-        out[:, i] = savitzky_golay(y[:, i], window_size, order, deriv)
+        out[:, i] = coeffs.dot(y[:, i])
     return out
-    
+
+import scipy.ndimage as nd
 def apply_sg_scan(y, window_size, order, deriv=0):
     out = np.zeros_like(y)
-    for s in range(y.shape[-1]):
-        for i in range(y.shape[1]):
-            out[:, i, s] = savitzky_golay(y[:, i, s], window_size, order, deriv)
+    print y.shape
+    c = sig.savgol_coeffs(window_size, order, deriv=0)
+#    for s in range(y.shape[-1]):
+#        for i in range(y.shape[1]):
+#            print c.shape
+    out = nd.convolve1d(y, c, 0, mode='nearest')
+     #out, s] = c.dot(y[:, i, 1, s])
     return out
 
 def calc_error(args):
@@ -415,8 +361,8 @@ def do_nnls(A,b):
     return out
 
 import lmfit
-def exp_fit(x, y, start_taus = [1], use_constant=True, amp_max=None):
-    print y.dtype
+def exp_fit(x, y, start_taus = [1], use_constant=True, amp_max=None,
+            verbose=True):
     num_exp = len(start_taus)
     para = lmfit.Parameters()
     if use_constant:
@@ -450,7 +396,8 @@ def exp_fit(x, y, start_taus = [1], use_constant=True, amp_max=None):
 
 
     mini = lmfit.minimize(res, para)
-    lmfit.report_errors(para)
+    if verbose:
+        lmfit.report_errors(para)
     y_fit = fit(para)
     return mini, y_fit 
 
