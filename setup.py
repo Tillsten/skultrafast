badb0117ef66c42c6fccda2f973b131225a413ba
tillsten <mail.till@gmx.de>
2018-05-17 14:51:21 +0200

    Fix missing line in dv, add test to import

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='skultrafast',
---
+++ tests/test_imports.py
@@ -0,0 +1,4 @@
+def test_import():
+    from skultrafast import dv, data_io, plot_helpers, ir_helpers
+    return
+    
