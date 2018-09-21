d72123dfa3cf5b8469db087a776fb92edca2dd65
Till S <mail.till@gmx.de>
2018-09-21 15:30:44 +0200

    Working cleaning command for setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,41 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, Command
+import os, glob, shutil
+from os.path import normpath, abspath, dirname, join
+here = normpath(abspath(dirname(__file__)))
 import versioneer
 
+here = normpath(abspath(dirname(__file__)))
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info ./__pycache__'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        global here
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob.glob(normpath(join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute + outside this directory
+                    raise ValueError("%s is not a path inside %s" % (path, here))
+                print('removing %s' % os.path.relpath(path))
+                shutil.rmtree(path)
+
+
+cmdclass = {'clean': CleanCommand}
+cmdclass.update(versioneer.get_cmdclass())
+
+
 setup(
-    cmdclass=versioneer.get_cmdclass(),
+    cmdclass=cmdclass,
     name='skultrafast',
     version=versioneer.get_version(),
     author='Till Stensitzki',
