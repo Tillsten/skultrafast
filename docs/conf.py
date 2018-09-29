dc5b9acfdf3698202ef7ef80eefa640436f3cf00
Till Stensitzki <mail.till@gmx.de>
2018-09-29 18:10:02 +0200

    Add readthedocs config

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,7 @@ version = '.'.join(release.split('.')[:2])
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
+default_role = 'any'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
