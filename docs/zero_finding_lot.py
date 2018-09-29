8af3810f536bbc501fccf3f524c4a3e275fd7ea9
Till Stensitzki <mail.till@gmx.de>
2018-09-30 00:28:56 +0200

    More zerofinding docs

--- docs/zero_finding_lot.py
+++ docs/zero_finding_lot.py
@@ -11,16 +11,16 @@ import skultrafast.data_io
 from skultrafast import zero_finding
 from scipy.ndimage import gaussian_filter1d
 wl, t, dat = skultrafast.data_io.load_example()
+plt.rcParams['font.family'] = 'Georgia'
 
-
-gw = {'height_ratios': (3,2,2)}
+gw = {'height_ratios': (3, 2, 2)}
 fig, axs = plt.subplots(3, 1, num='test', figsize=(5, 6), gridspec_kw=gw,
                         sharex='col', constrained_layout=True)
 
 t = t*1000
 y = dat[:, 200]
 axs[0].plot(t, dat[:, 200], '-sk', mec='w', ms=4)
-axs[0].set_xlim(-.5*1000,1.5*1000)
+axs[0].set_xlim(-.5*1000, 1.5*1000)
 axs[-1].set_xlabel('Time [fs]')
 axs[0].set_ylabel('mOD')
 
@@ -30,7 +30,7 @@ axs[1].set_ylabel('diff')
 axs[2].plot(t, gaussian_filter1d(y, 1.5, order=1), '-o', c='C1')
 axs[2].set_ylabel('gauss diff')
 axs[2].text(-450, -1.2, 'sigma=1.5',)
-axs[0].text(750, -.7, 'val=2',color='C4')
+axs[0].text(750, -.7, 'val=2', color='C4')
 td = zero_finding.use_diff(dat[:, 200])
 tm = np.argmax(y)
 tg = zero_finding.use_gaussian(dat, sigma=1)[200]
@@ -44,5 +44,3 @@ axs[0].plot(t[ta], y[ta], '>', ms=10, zorder=.9, c='C4', label='use_first_abs')
 axs[0].axhline(-2, alpha=0.5, c='C4')
 axs[0].axhline(2, alpha=0.5, c='C4')
 axs[0].legend()
-fig.suptitle('Heuristics')
-plt.savefig('zero_finding_heuristics.svg')
