Fix masking, more tests

skultrafast/dataset.py
@@ -208,8 +208,7 @@ class TimeResSpec:
 
         Returns
         -------
-        : TimeResSpec
-            TimeResSpec containing only the listed regions.
+        : None
         """
         idx = np.zeros_like(self.wavelengths, dtype=np.bool)
         if freq_unit is None:
@@ -218,10 +217,11 @@ class TimeResSpec:
 
         for (lower, upper) in freq_ranges:
             idx ^= np.logical_and(arr > lower, arr < upper)
-        if not invert_sel:
+        if invert_sel:
             idx = ~idx
         if self.err is not None:
-            self.err.mask[:, idx] = True
+            self.err = np.ma.MaskedArray(self.err)
+            self.err[:, idx] = np.ma.masked
         self.data = np.ma.MaskedArray(self.data)
         self.data[:, idx] = np.ma.masked
 
@@ -590,7 +590,6 @@ def delgator(pol_tr : PolTRSpec, method : typing.Callable):
         method returns a new TimeResSpec.
     """
     name = method.__name__
-    print(method)
     hints = typing.get_type_hints(method)
     if 'return' in hints:
         do_return =  hints['return'] == TimeResSpec

skultrafast/tests/test_dataset.py
@@ -2,6 +2,7 @@
 from skultrafast.dataset import TimeResSpec, PolTRSpec
 from skultrafast.data_io import  load_example
 import numpy as np
+from numpy.testing import assert_almost_equal
 
 wl, t, data = load_example()
 
@@ -14,14 +15,27 @@ def test_methods():
     nds = ds.cut_times([(-100, 1)])
     assert(np.all(nds.t > .99))
     nds = ds.bin_times(5)
+    assert(nds.t.size == np.ceil(ds.t.size/5))
+    ds.mask_freqs([(400, 600)])
+    assert(np.all(ds.data.mask[:, ds.wl_idx(550)]))
+
 
 def test_pol_tr():
     ds = TimeResSpec(wl, t, data)
-    ps = PolTRSpec(para=ds, perp=ds)
-    ps.bin_freqs(10)
+    ds2 = TimeResSpec(wl, t, data)
+    ps = PolTRSpec(para=ds, perp=ds2)
+    out = ps.bin_freqs(10)
+    assert(out.para.wavenumbers.size == 10)
+    assert(out.perp.wavenumbers.size == 10)
+    assert_almost_equal(out.perp.data, out.para.data)
     ps.subtract_background()
-    ps.mask_freqs([(500, 550)])
-    ps.cut_freqs([(500, 550)])
+    ps.mask_freqs([(400, 550)])
+    print(ps.para.data.mask, ps.para.data.mask[1, ps.para.wl_idx(520)])
+
+    assert(ps.para.data.mask[1, ps.para.wl_idx(520)] )
+    out = ps.cut_freqs([(400, 550)])
+    assert(np.all(out.para.wavelengths >= 550))
+    assert(np.all(out.perp.wavelengths >= 550))
     ps.bin_times(6)
 
 def test_plot():