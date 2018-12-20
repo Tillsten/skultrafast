a2853dfb89aa40623f8dfd66c9f11236834b8881
Till Stensitzki <mail.till@gmx.de>
2018-12-20 17:02:14 +0100

    fix unit tests

--- skultrafast/tests/test_units.py
+++ skultrafast/tests/test_units.py
@@ -10,7 +10,7 @@ def test_roundtrip():
 
 def test_handle_np():
     x = np.arange(2, 30, dtype='float')
-    assert eV2cm(cm2eV(x)) == x
-    assert cm2fs(fs2cm(x)) == x
-    assert cm2THz(THz2cm(x)) == x
-    assert cm2nm(nm2cm(x)) == x
+    np.testing.assert_allclose(eV2cm(cm2eV(x)), x)
+    np.testing.assert_allclose(cm2fs(fs2cm(x)), x)
+    np.testing.assert_allclose(cm2THz(THz2cm(x)),x)
+    np.testing.assert_allclose(cm2nm(nm2cm(x)), x)
