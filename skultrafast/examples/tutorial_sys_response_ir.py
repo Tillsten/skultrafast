b4be4ab3fc51ea05ff81b45c0eb2a8927b7ffe24
tillsten <mail.till@gmx.de>
2019-11-29 17:12:54 +0100

    [Doc] Added get_t0 tutorial

---
+++ skultrafast/examples/tutorial_sys_response_ir.py
@@ -0,0 +1,21 @@
+"""
+Measuring the system response in the mid IR
+===========================================
+
+In the mid-IR the system response is measured by monitoring the transmittance of the
+probe light through a thin semi-conductor. skultrafast has an helper function to
+analyze such a signal.
+"""
+# %%
+from skultrafast import messpy, data_io
+
+fname = data_io.get_example_path('sys_response')
+center, sigma, lmfit_model, fig = messpy.get_t0(fname, display_result=False,
+                                                t_range=(-2, 0.3),
+                                                no_slope=False)
+
+# %%
+# Newer version of lmfit have a html representation which is used by ipython, e.g.
+# in the notebook. Hence the line below will show the fit results.
+
+lmfit_model
