fd59052e2ad0456e8b93d668c16282896c748639
Till Stensitzki <mail.till@gmx.de>
2021-11-15 13:29:38 +0100

    more 2d tests

---
+++ skultrafast/tests/test_twodim.py
@@ -0,0 +1,53 @@
+import pytest
+
+from skultrafast.quickcontrol import QC2DSpec
+from skultrafast.data_io import get_twodim_dataset
+from skultrafast.twoD_dataset import TwoDim
+from pathlib import Path
+
+
+@pytest.fixture(scope='session')
+def datadir2d(tmp_path_factory):
+    p = get_twodim_dataset()
+    return p
+
+
+@pytest.fixture(scope='session')
+def two_d(datadir2d) -> TwoDim:
+    info = list(Path(datadir2d).glob('*320.info'))
+    qc = QC2DSpec(info[0])
+    ds = qc.make_ds()["iso"]
+    return ds
+
+
+def test_select(two_d):
+    two_d = two_d.copy()
+    two_d = two_d.select_range((2030, 2200), (2030, 2200))
+
+    assert two_d.pump_wn.min() > 2030
+    assert two_d.pump_wn.size > 0
+    assert two_d.pump_wn.max() < 2200
+
+    assert two_d.probe_wn.min() > 2030
+    assert two_d.probe_wn.size > 0
+    assert two_d.probe_wn.max() < 2200
+
+
+    two_d = two_d.select_t_range(1)
+
+    assert two_d.t.min() > 1
+
+
+@pytest.fixture(scope='session')
+def two_d_processed(two_d):
+    two_d = two_d.copy()
+    two_d.select_range()
+
+def test_integrate(two_d):
+    two_d = two_d.copy()
+    two_d.intregrate_pump()
+
+
+
+def test_cls(two_d):
+    two_d = two_d.copy()
