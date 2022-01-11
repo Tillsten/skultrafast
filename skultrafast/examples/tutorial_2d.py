126288d00877c37fd5f8b2ad6715ded40e10c92c
tillsten <mail.till@gmx.de>
2022-01-11 18:12:24 +0100

    Text updates

--- skultrafast/examples/tutorial_2d.py
+++ skultrafast/examples/tutorial_2d.py
@@ -2,10 +2,11 @@
 2D Spectroscopy
 ===============
 
-In this example we show skultrafasts current capabilties when it comes to
-working with 2D-spectra. First we need to get a sample spectrum. For that, we
-use example data, which will be downloaded from figshare if necessary. The data
-was measured with the quickcontrol-software from phasetech.
+This example demonstrates _skultrafasts_ current capabilties when it comes to
+working with 2D-spectra. First, we need a sample spectrum. For that,
+_skultrafast_ can access example data which is hosted on figshare and will be
+downloaded if necessary. The data was measured with the quickcontrol-software
+from phasetech.
 
 We start by import the required functionalty.
 """
@@ -41,7 +42,7 @@ infos
 # ----------------
 #
 # There are two `.info`-files the directory. The first contains the transient
-# 1D-data and the second the 2d-data. Here in this tutorial we will work with
+# 1D-data and the second the transient 2D-data. Here in this tutorial we will work with
 # the 2D data. Hence we select the second file and open it by instancing an
 # `QC2DSpec`-class. Given the info-file, the class collects all necessary data.
 # It is also responsible to turn the saved data, which are inferogramms, into
@@ -54,8 +55,8 @@ plot_helpers.enable_style()
 qc_file = QC2DSpec(infos[1], bg_correct=(10, 10), upsampling=4)
 
 # %%
-# To create the dataset to work with form raw data we call the make_ds methods,
-# which returns a dict of `TwoDim` objects to work with. The dict contains
+# To create a dataset to work with form the raw data, we call the `make_ds`
+# method. The method returns a dict of `TwoDim` objects to work with, containing
 # parallel (`para`), perpendicular (`perp`) and isotropic (`iso`) datasets. We
 # select the isotropic dataset.
 
@@ -64,7 +65,7 @@ ds_iso = ds_all['iso']
 
 # %%
 # The `TwoDim`-objects are the core structure to work with. They contain both
-# frequency-axis, `pump_freqs` and `probe_freqs`, the waiting times `t`, and the
+# frequency-axes, `pump_freqs` and `probe_freqs`, the waiting times `t`, and the
 # actual data `spec2d`.
 #
 # To start and to get an overview, we plot a contour-plot at 1 ps. Like for the
@@ -92,11 +93,11 @@ c, _ = ds.plot.contour(0.5, 1, 7, aspect=1, direction='h')
 # Extracting a transient 1D-spectrum
 # ----------------------------------
 #
-# Using the projection theorem and integrating over the pump axis, we can
-# get a normal transient 1D-dataset. This can be done by the
-# `TwoDim.integrate_method`, which returns a skultrafast `TimeResSpec`. Here we
-# integrate over the whole range. It is possible to integrate over a sub-range
-# by supplying arguments to the function.
+# Using the projection theorem by integrating over the pump axis, we can
+# calculate a normal transient 1D-dataset. In _skultrafast_, this is archived by
+# the `TwoDim.integrate_method`, which returns a skultrafast `TimeResSpec`. Here
+# we integrate over the whole range. It is possible to integrate over a
+# sub-range by supplying arguments to the function.
 
 ds1d = ds.integrate_pump()
 
@@ -109,10 +110,10 @@ ds1d.plot.trans(2160, 2135, add_legend=True, ax=ax1)
 # Center line slope analysis
 # --------------------------
 #
-# One of the most common ways to analyze a two-dimensional dataset is to
-# extract the frequency-frequency correlation function (FFCF). The most common
-# ways is to determine the center line slope, which under certain assumptions is
-# proportional to the normalized FFCF.
+# One of the most common ways to analyze a two-dimensional dataset is to measure
+# the frequency-frequency correlation function (FFCF). The most common ways for
+# extraction is to determine the center line slope decay, which under certain
+# assumptions is proportional to the FFCF.
 #
 # To extract the cls for a single time-point, we use the `single_cls`-method.
 # Let's determine the cls for 1 ps. We use a window of 10 cm-1 in both pump and
@@ -196,8 +197,8 @@ fig, ax = plt.subplots()
 pump_range = 5, 7, 10, 12,
 for r in pump_range:
     cls_result_fit = ds.cls(pr_range=r, pu_range=r, method='com')
-
-    cls_result_fit.plot_cls(ax=ax, symlog=True)
+    data_line, _ = cls_result_fit.plot_cls(ax=ax, symlog=True)
+    data_line.set_label(r)
 
 ax.legend()
 
@@ -207,15 +208,16 @@ ax.legend()
 #
 # Another common usage of two-dimensional spectra is the extraction of the
 # diagonal. The signal on diagonal is proportional to the fourth power of the
-# transition dipole-moment, in contrast to the regular absorption
-# (e.g. FTIR) spectrum, which is  proportional to the second power. Hence, shoulders
-# of peaks are often more distinct in the diagonal. The diagonal can be extracted via the
-# 'diag_and_antidag'-method. The methods returns an object containing both the
-# diagonal and anti-diagonal, additional it contains the y-coordinates of the
-# lines. Latter can be useful for plotting. Again the method takes a waiting time
-# to select the spectrum for extraction. Additionally, it takes parameters to shift
-# the position of the diagonal used for extraction. If not given, it goes through
-# the position of the minimum.
+# transition dipole-moment, in contrast to the regular absorption (e.g. FTIR)
+# spectrum, which is  proportional to the second power. Hence, shoulders of
+# peaks with different excitation coefficents are often more distinct in the
+# diagonal. The diagonal can be extracted via the 'diag_and_antidag'-method. The
+# methods returns an object containing both the diagonal and anti-diagonal,
+# additional it contains the y-coordinates of the lines. Latter can be useful
+# for plotting. Again the method takes a waiting time to select the spectrum for
+# extraction. Additionally, it takes parameters to shift the position of the
+# diagonal used for extraction. If not given, it goes through the position of
+# the minimum.
 
 diag_result = ds.diag_and_antidiag(0.2)
 
@@ -243,7 +245,8 @@ ax['B'].set_xlabel(plot_helpers.freq_label)
 
 fig.align_xlabels()
 # %%
-# As an alternative, the pump-slice-amplitude method is also supported. Here we are calculating the
-# difference between the maximum and minimum signal along the probe axis for each pump frequency.
-# The resulting curve shares the same properties as the diagonal but is supposedly less influenced
-# by excited state overlap (Valentine et al. doi:10.1021/acs.jpca.1c04558)
+# As an alternative, the pump-slice-amplitude method is also supported. Here we
+# are calculating the difference between the maximum and minimum signal along
+# the probe axis for each pump frequency. The resulting curve shares the same
+# properties as the diagonal but is supposedly less influenced by excited state
+# overlap (Valentine et al. doi:
