Allow normalization for a wavelength, fix err cls

skultrafast/twoD_dataset.py
@@ -299,7 +299,10 @@ class TwoDim:
                 amp = np.trapz(s[pr_idx], pr[pr_idx])
                 result = mod.fit(s[pr_idx], sigma=3, center=cen_of_m, amplitude=amp, c=0,
                                  x=pr[pr_idx])
-                l.append((result.params['center'], result.params['center'].stderr))
+                val, err = (result.params['center'].value, result.params['center'].stderr)
+                if err is None:
+                    err = np.nan
+                l.append((val, err))
             elif method == 'quad':
                 p: Polynomial = Polynomial.fit(pr[pr_idx], s[pr_idx], 2)  # type: Ignore
                 l.append((p.deriv().roots()[0], 1))
@@ -314,7 +317,10 @@ class TwoDim:
                 amp = np.trapz(s[pr_idx], pr[pr_idx])
                 result = mod.fit(s[pr_idx], sigma=3, center=cen_of_m, amplitude=amp,
                                  x=pr[pr_idx], slope=0, intercept=0)
-                l.append((result.params['center'], result.params['center'].stderr))
+                val, err = (result.params['center'].value, result.params['center'].stderr)
+                if err is None:
+                    err = np.nan
+                l.append((val, err))
 
             else:
                 l.append((cen_of_m, 1))
@@ -500,4 +506,3 @@ class TwoDim:
         psamax = self.pump_wn[self.pump_slice_amp(t).argmax()]
         return {'ProbeMin': probe_min, 'ProbeMax': probe_max, 'PSAMax': psamax,
                 'PumpMin': pump_min, 'PumpMax': pump_max}
-                

skultrafast/twoD_plotter.py
@@ -1,12 +1,14 @@
-from skultrafast import plot_helpers
-from skultrafast.utils import inbetween
+from typing import (TYPE_CHECKING, Any, Dict, List, Literal, Optional, Tuple,
+                    TypedDict, Union)
 
 import attr
-import numpy as np
-from scipy.ndimage import uniform_filter1d
-from scipy.interpolate import RegularGridInterpolator
-from typing import Any, TypedDict, Union, Literal, Optional, Tuple, TYPE_CHECKING, Dict
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+from scipy.ndimage import uniform_filter1d
+
+from skultrafast import plot_helpers
+from skultrafast.utils import inbetween
 
 if TYPE_CHECKING:
     from skultrafast.twoD_dataset import TwoDim
@@ -49,7 +51,7 @@ class TwoDimPlotter:
                 ax_size_x = ax_size*aspect
                 ax_size_y = ax_size
 
-            fig, ax = plot_helpers.fig_fixed_axes((nrows, ncols), (ax_size_y, ax_size_x), # typing: ignore
+            fig, ax = plot_helpers.fig_fixed_axes((nrows, ncols), (ax_size_y, ax_size_x),  # typing: ignore
                                                   xlabel=labels['x'], ylabel=labels['y'], left_margin=0.7, bot_margin=0.6,
                                                   hspace=0.15, vspace=0.15, padding=0.3, **fig_kws)
 